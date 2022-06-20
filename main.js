035e06790e080ba908cdff9e18144cc332881b52
Kiks <kevin.kiks.richard@gmail.com>
2022-06-20 15:38:41 +0200

    tests

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let output = document.getElementById('output');
 let clearBtn = document.getElementById('clearbtn');
 let nbClick=0;
 let nbClickMax=2;
+let apiKey = '4c0665f89ed0153262e09818da099b8d';
+let city = input.value;
 
 /* ----- Functions autofocus & fill the input ----- */
 
@@ -22,83 +24,6 @@ input.addEventListener('blur', function() {
         input.value = 'Enter a city';
     }
     }
-);
-
-/* ----- Get the weather ----- */
-
-button.addEventListener('click', function() {
-    let city = input.value;
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=4c0665f89ed0153262e09818da099b8d';
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        }
-        )
-        .then(function(data) {
-            /* ----- Create variables ----- */
-            let weather = data.weather[0].main;
-            let temp = data.main.temp;
-            let tempCelsius = temp - 273.15;
-            let tempFahrenheit = temp * 9/5 - 459.67;
-            let tempCelsiusRound = Math.round(tempCelsius);
-            let tempFahrenheitRound = Math.round(tempFahrenheit);
-            let city = data.name;
-            let country = data.sys.country;
-            let icon = data.weather[0].icon;
-            let iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
-            let iconImg = document.createElement('img');
-
-            iconImg.src = iconUrl;
-            iconImg.alt = weather;
-
-            let iconImgContainer = document.createElement('div');
-            iconImgContainer.classList.add('iconImgContainer');
-            iconImgContainer.appendChild(iconImg);
-
-            let cityContainer = document.createElement('div');
-            cityContainer.classList.add('cityContainer');
-            cityContainer.innerHTML = city + ', ' + country;
-
-            let weatherContainer = document.createElement('div');
-            weatherContainer.classList.add('weatherContainer');
-            weatherContainer.innerHTML = weather;
-
-            let tempContainer = document.createElement('div');
-            tempContainer.classList.add('tempContainer');
-            tempContainer.innerHTML = tempCelsiusRound + '&deg;C / ' + tempFahrenheitRound + '&deg;F';
-
-            let cityCard = document.createElement('div');
-            cityCard.classList.add('cityCard');
-
-            cityCard.appendChild(cityContainer);
-            cityCard.appendChild(weatherContainer);
-            cityCard.appendChild(tempContainer);
-            cityCard.appendChild(iconImgContainer);
-
-            output.appendChild(cityCard);
-
-            // i want to change the the bg color depending on the alt of the img
-
-                if (iconImg.alt === 'Clear') {
-                    cityCard.style.backgroundColor = 'rgb(102,187,221)';
-                } else if (iconImg.alt === 'Clouds') {
-                    cityCard.style.backgroundColor = 'rgb(160,160,160)';
-                } else if (iconImg.alt === 'Rain') {
-                    cityCard.style.backgroundColor = '#9099a1';
-                } else if (iconImg.alt === 'Snow') {
-                    cityCard.style.backgroundColor = '#fffafa';
-                } else if (iconImg.alt === 'Mist', 'Haze') {
-                    cityCard.style.backgroundColor = '#e6e6fa';
-                } else if (iconImg.alt === 'Thunderstorm') {
-                    cityCard.style.backgroundColor = '#001C3D';
-                }
-        }
-        )
-        .catch(function(error) {
-            console.log(error);
-        }
-        );
-}
 );
 
 /* Press Enter to get the weather */
@@ -128,3 +53,138 @@ if(nbClick>=nbClickMax)
     }
 }
 
+/* call the geocoding API */
+function getGeocoding(city) {
+    let url = 'https://api.opencage.com/geocode/v1/json?q=' + city + '&key=' + apiKey;
+    return fetch(url)
+        .then(function(response) {
+            return response.json();
+        }
+        )
+        .catch(function(error) {
+            console.log(error);
+        }
+        );
+}
+
+/* call the weather API  that displays the weather of the city for the next 5 days */
+function getWeather(lat, lng) {
+    let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lng + '&units=metric&appid=' + apiKey;
+    return fetch(url)
+        .then(function(response) {
+            return response.json();
+        }
+        )
+        .catch(function(error) {
+            console.log(error);
+        }
+        );
+}
+
+/* display the weather of the city in a card in the output */
+function displayWeather(weather) {
+    let cityContainer = document.createElement('div');
+    cityContainer.classList.add('cityContainer');
+
+    let weatherContainer = document.createElement('div');
+    weatherContainer.classList.add('weatherContainer');
+
+    let tempContainer = document.createElement('div');
+    tempContainer.classList.add('tempContainer');
+
+    let iconImgContainer = document.createElement('div');
+    iconImgContainer.classList.add('iconImgContainer');
+
+    let cityCard = document.createElement('div');
+    cityCard.classList.add('cityCard');
+
+    let cityName = document.createElement('h2');
+    cityName.classList.add('cityName');
+    cityName.innerHTML = weather.city.name;
+
+    let temp = document.createElement('p');
+    temp.classList.add('temp');
+    temp.innerHTML = weather.list[0].main.temp + '°C';
+
+    let iconImg = document.createElement('img');
+    iconImg.classList.add('iconImg');
+    iconImg.src = 'http://openweathermap.org/img/wn/' + weather.list[0].weather[0].icon + '.png';
+
+    let weatherDesc = document.createElement('p');
+    weatherDesc.classList.add('weatherDesc');
+    weatherDesc.innerHTML = weather.list[0].weather[0].description;
+
+    let weatherDate = document.createElement('p');
+    weatherDate.classList.add('weatherDate');
+    weatherDate.innerHTML = 'Date: ' + weather.list[0].dt_txt;
+
+    let weatherTemp = document.createElement('p');
+    weatherTemp.classList.add('weatherTemp');
+    weatherTemp.innerHTML = 'Temperature: ' + weather.list[0].main.temp + '°C';
+
+    let weatherHumidity = document.createElement('p');
+    weatherHumidity.classList.add('weatherHumidity');
+    weatherHumidity.innerHTML = 'Humidity: ' + weather.list[0].main.humidity + '%';
+
+    let weatherWind = document.createElement('p');
+    weatherWind.classList.add('weatherWind');
+    weatherWind.innerHTML = 'Wind: ' + weather.list[0].wind.speed + 'km/h';
+
+    let weatherClouds = document.createElement('p');
+    weatherClouds.classList.add('weatherClouds');
+    weatherClouds.innerHTML = 'Clouds: ' + weather.list[0].clouds.all + '%';
+
+    let weatherPressure = document.createElement('p');
+    weatherPressure.classList.add('weatherPressure');
+    weatherPressure.innerHTML = 'Pressure: ' + weather.list[0].main.pressure + 'hPa';
+
+    let weatherSunrise = document.createElement('p');
+    weatherSunrise.classList.add('weatherSunrise');
+    weatherSunrise.innerHTML = 'Sunrise: ' + weather.city.sunrise;
+
+    let weatherSunset = document.createElement('p');
+    weatherSunset.classList.add('weatherSunset');
+    weatherSunset.innerHTML = 'Sunset: ' + weather.city.sunset;
+
+    cityContainer.appendChild(cityCard);
+    cityCard.appendChild(cityName);
+    cityCard.appendChild(tempContainer);
+    tempContainer.appendChild(temp);
+    tempContainer.appendChild(iconImgContainer);
+    iconImgContainer.appendChild(iconImg);
+    cityCard.appendChild(weatherContainer);
+    weatherContainer.appendChild(weatherDesc);
+    weatherContainer.appendChild(weatherDate);
+    weatherContainer.appendChild(weatherTemp);
+    weatherContainer.appendChild(weatherHumidity);
+    weatherContainer.appendChild(weatherWind);
+    weatherContainer.appendChild(weatherClouds);
+    weatherContainer.appendChild(weatherPressure);
+    weatherContainer.appendChild(weatherSunrise);
+    weatherContainer.appendChild(weatherSunset);
+
+    output.appendChild(cityContainer);
+}
+
+/* ----- Get the weather of the city ----- */
+
+button.addEventListener('click', function() {
+    let city = input.value;
+    getGeocoding(city)
+        .then(function(response) {
+            let lat = response.results[0].geometry.lat;
+            let lng = response.results[0].geometry.lng;
+            return getWeather(lat, lng);
+        }
+        )
+        .then(function(response) {
+            displayWeather(response);
+            compter();
+        }
+        )
+        .catch(function(error) {
+            console.log(error);
+        }
+        );
+}
+);
---
+++ test.js
@@ -0,0 +1,215 @@
+/* ----- Variables ----- */
+
+let input = document.getElementById('cityInput');
+let button = document.getElementById('btn');
+let body = document.querySelector('body');
+let output = document.getElementById('output');
+let clearBtn = document.getElementById('clearbtn');
+let nbClick=0;
+let nbClickMax=2;
+let apiKey = '4c0665f89ed0153262e09818da099b8d';
+
+/* ----- Functions autofocus & fill the input ----- */
+
+input.addEventListener('click', function() {
+    if (input.value === 'Enter a city') {
+        input.value = '';
+    }
+    }
+);
+
+input.addEventListener('blur', function() {
+    if (input.value === '') {
+        input.value = 'Enter a city';
+    }
+    }
+);
+
+/* ----- Get the weather ----- */
+
+// button.addEventListener('click', function() {
+//     let city = input.value;
+//     let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+//     fetch(url)
+//         .then(function(response) {
+//             return response.json();
+//         }
+//         )
+//         .then(function(data) {
+//             /* ----- Create variables ----- */
+//             let weather = data.weather[0].main;
+//             let temp = data.main.temp;
+//             let tempCelsius = temp - 273.15;
+//             let tempFahrenheit = temp * 9/5 - 459.67;
+//             let tempCelsiusRound = Math.round(tempCelsius);
+//             let tempFahrenheitRound = Math.round(tempFahrenheit);
+//             let city = data.name;
+//             let country = data.sys.country;
+//             let icon = data.weather[0].icon;
+//             let iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
+//             let iconImg = document.createElement('img');
+
+//             iconImg.src = iconUrl;
+//             iconImg.alt = weather;
+
+//             let iconImgContainer = document.createElement('div');
+//             iconImgContainer.classList.add('iconImgContainer');
+//             iconImgContainer.appendChild(iconImg);
+
+//             let cityContainer = document.createElement('div');
+//             cityContainer.classList.add('cityContainer');
+//             cityContainer.innerHTML = city + ', ' + country;
+
+//             let weatherContainer = document.createElement('div');
+//             weatherContainer.classList.add('weatherContainer');
+//             weatherContainer.innerHTML = weather;
+
+//             let tempContainer = document.createElement('div');
+//             tempContainer.classList.add('tempContainer');
+//             tempContainer.innerHTML = tempCelsiusRound + '&deg;C / ' + tempFahrenheitRound + '&deg;F';
+
+//             let cityCard = document.createElement('div');
+//             cityCard.classList.add('cityCard');
+
+//             cityCard.appendChild(cityContainer);
+//             cityCard.appendChild(weatherContainer);
+//             cityCard.appendChild(tempContainer);
+//             cityCard.appendChild(iconImgContainer);
+
+//             output.appendChild(cityCard);
+
+//             // i want to change the the bg color depending on the alt of the img
+
+//                 if (iconImg.alt === 'Clear') {
+//                     cityCard.style.backgroundColor = 'rgb(102,187,221)';
+//                 } else if (iconImg.alt === 'Clouds') {
+//                     cityCard.style.backgroundColor = 'rgb(160,160,160)';
+//                 } else if (iconImg.alt === 'Rain') {
+//                     cityCard.style.backgroundColor = '#9099a1';
+//                 } else if (iconImg.alt === 'Snow') {
+//                     cityCard.style.backgroundColor = '#fffafa';
+//                 } else if (iconImg.alt === 'Mist', 'Haze') {
+//                     cityCard.style.backgroundColor = '#e6e6fa';
+//                 } else if (iconImg.alt === 'Thunderstorm') {
+//                     cityCard.style.backgroundColor = '#001C3D';
+//                 }
+//         }
+//         )
+//         .catch(function(error) {
+//             console.log(error);
+//         }
+//         );
+// }
+// );
+
+/* Press Enter to get the weather */
+
+input.addEventListener('keyup', function(event) {
+    if (event.key === "Enter") {
+        button.click();
+    }
+});
+
+/* ----- Clear the output ----- */
+
+clearBtn.addEventListener('click', function() {
+    output.innerHTML = '';
+    nbClick=0;
+    document.getElementById('btn').disabled=false;
+});
+
+/* ----- limitation du nombre de clic ----- */
+
+function compter(){
+nbClick++;
+if(nbClick>=nbClickMax)
+    {
+        alert('You can only choose ' + nbClickMax + ' cities');
+        document.getElementById('btn').disabled=true;
+    }
+}
+
+/* afficher 5 jours suivant */
+button.addEventListener('click', function() {
+    compter();
+    if(nbClick<nbClickMax)
+    {
+        let city = input.value;
+        let url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            }
+            )
+            .then(function(data) {
+                /* ----- Create variables ----- */
+                let weather = data.weather[0].main;
+                let temp = data.main.temp;
+                let tempCelsius = temp - 273.15;
+                let tempFahrenheit = temp * 9/5 - 459.67;
+                let tempCelsiusRound = Math.round(tempCelsius);
+                let tempFahrenheitRound = Math.round(tempFahrenheit);
+                let city = data.name;
+                let country = data.sys.country;
+                let icon = data.weather[0].icon;
+                let iconUrl = 'http://openweathermap.org/img/w/' + icon + '.png';
+                let iconImg = document.createElement('img');
+
+                iconImg.src = iconUrl;
+                iconImg.alt = weather;
+
+                let iconImgContainer = document.createElement('div');
+                iconImgContainer.classList.add('iconImgContainer');
+                iconImgContainer.appendChild(iconImg);
+                
+                let cityContainer = document.createElement('div');
+                cityContainer.classList.add('cityContainer');
+                cityContainer.innerHTML = city + ', ' + country;
+                
+                let weatherContainer = document.createElement('div');
+                weatherContainer.classList.add('weatherContainer');
+                weatherContainer.innerHTML = weather;
+
+                let tempContainer = document.createElement('div');
+                tempContainer.classList.add('tempContainer');
+                tempContainer.innerHTML = tempCelsiusRound + '&deg;C / ' + tempFahrenheitRound + '&deg;F';
+
+                let cityCard = document.createElement('div');
+                cityCard.classList.add('cityCard');
+
+                cityCard.appendChild(cityContainer);
+                cityCard.appendChild(weatherContainer);
+                cityCard.appendChild(tempContainer);
+                cityCard.appendChild(iconImgContainer);
+
+                output.appendChild(cityCard);
+
+                // i want to change the the bg color depending on the alt of the img
+                
+                    if (iconImg.alt === 'Clear') {
+                        cityCard.style.backgroundColor = 'rgb(102,187,221)';
+                    }
+                    else if (iconImg.alt === 'Clouds') {
+                        cityCard.style.backgroundColor = 'rgb(160,160,160)';
+                    }
+                    else if (iconImg.alt === 'Rain') {
+                        cityCard.style.backgroundColor = '#9099a1';
+                    }
+                    else if (iconImg.alt === 'Snow') {
+                        cityCard.style.backgroundColor = '#fffafa';
+                    }
+                    else if (iconImg.alt === 'Mist', 'Haze') {
+                        cityCard.style.backgroundColor = '#e6e6fa';
+                    }
+                    else if (iconImg.alt === 'Thunderstorm') {
+                        cityCard.style.backgroundColor = '#001C3D';
+                    }
+            }
+            )
+            .catch(function(error) {
+                console.log(error);
+            }
+            );
+    }
+}
+);
